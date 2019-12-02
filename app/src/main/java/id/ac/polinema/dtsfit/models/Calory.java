3846849daa480af4fed7f07b99f6f79059e05c55
mohkhoirularifin <karifin998@gmail.com>
2019-12-02 15:01:26 +0700

    6 - Implementasi CaloryFragment

--- app/src/main/java/id/ac/polinema/dtsfit/models/Calory.java
+++ app/src/main/java/id/ac/polinema/dtsfit/models/Calory.java
@@ -1,7 +1,55 @@
 package id.ac.polinema.dtsfit.models;
 
-// TODO: Implementasikan parcelable
-public class Calory {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+// TODO: Implementasikan parcelable
+public class Calory implements Parcelable {
     // TODO: Generate model berdasarkan response REST
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("food")
+    @Expose
+    private String food;
+    @SerializedName("calory")
+    @Expose
+    private Integer calory;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public Integer getCalory() {
+        return calory;
+    }
+
+    public void setCalory(Integer calory) {
+        this.calory = calory;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
 }
